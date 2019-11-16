855fd9f7ae6e97ed25e094083bd5c2e4e45e087f
Koki Takahashi <hakatasiloving@gmail.com>
2019-11-16 19:02:52 +0900

    Implement tsglive endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as firebase from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import express from 'express';
+import cors from 'cors';
 
 firebase.initializeApp();
 const db = firebase.firestore();
@@ -18,7 +20,7 @@ export const updateCounts = functions.firestore.document('achievements/{id}').on
 
 		await transaction.update(achievementRef, {
 			count: (achievementDatum.get('count') || 0) + 1,
-			...(achievementDatum.get('first') === undefined ? {first: user} : {})
+			...(achievementDatum.get('first') === undefined ? {first: user} : {}),
 		});
 
 		const oldCounts = userDatum.get('counts') || {};
@@ -30,3 +32,21 @@ export const updateCounts = functions.firestore.document('achievements/{id}').on
 		});
 	});
 });
+
+const app = express();
+
+app.use(cors({origin: true}));
+
+app.get('/', (req, res) => {
+	res.send('Hello!');
+});
+
+app.post('/tahoiya', (req, res) => {
+	res.send('Hello!');
+});
+
+app.post('/comments', (req, res) => {
+	res.send('Hello!');
+});
+
+export const tsglive = functions.https.onRequest(app);
