9433b57b31a3d43e29120148e33bb24bd8c11a19
Koki Takahashi <hakatasiloving@gmail.com>
2020-03-25 13:31:16 +0900

    Change favicon style

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 			{hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
 		],
 		link: [
-			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+			{rel: 'icon', type: 'image/png', href: 'https://img.icons8.com/flat_round/512/trophy'},
 		],
 		htmlAttrs: {
 			lang: 'ja',
