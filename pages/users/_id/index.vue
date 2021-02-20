c0b38edb3412abe4cb2c33533cc65d761302eb77
Koki Takahashi <hakatasiloving@gmail.com>
2021-02-21 02:22:46 +0900

    Fix navigation error on locked achievements section

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -47,7 +47,7 @@
 		<p class="title">未解除の実績一覧</p>
 		<div class="columns is-multiline">
 			<div v-for="datum in lockedAchievements" :key="datum.name" class="column is-one-third">
-				<nuxt-link class="card" :to="`/achievements/${id}`" :style="{display: 'block'}">
+				<nuxt-link class="card" :to="`/achievements/${datum.name}`" :style="{display: 'block'}">
 					<div class="card-image">
 						<div class="image achievements-color" :style="{backgroundColor: getCategoryColor(datum.category)}"/>
 					</div>
