50d2322ef5f240891ea4e0f85584b9324166ac4f
Koki Takahashi <hakatasiloving@gmail.com>
2021-02-21 13:07:34 +0900

    Implement achievements stats calculation functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,21 +12,36 @@ export const updateCounts = functions_firestore.document('achievements/{id}').on
 	db.runTransaction(async (transaction) => {
 		const name = achievement.get('name');
 		const user = achievement.get('user');
+		const date = new Date(achievement.get('date').seconds);
+		const month = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}`;
 
 		const achievementRef = db.collection('achievement_data').doc(name);
-		const userRef = db.collection('users').doc(user);
+		const achievementDatum = await transaction.get(achievementRef);
 
-		const achievementDatum = await transaction.get(db.collection('achievement_data').doc(name));
-		const userDatum = await transaction.get(userRef);
 		const category = achievementDatum.get('category');
+		const difficulty = achievementDatum.get('difficulty');
 
-		await transaction.update(achievementRef, {
+		const userRef = db.collection('users').doc(user);
+		const categoryStatRef = db.collection('achievement_stats_by_category').doc(category);
+		const difficultyStatRef = db.collection('achievement_stats_by_difficulty').doc(difficulty);
+		const monthStatRef = db.collection('achievement_stats_by_month').doc(month);
+
+		const userDatum = await transaction.get(userRef);
+		const categoryStatDatum = await transaction.get(categoryStatRef);
+		const difficultyStatDatum = await transaction.get(difficultyStatRef);
+		const monthStatDatum = await transaction.get(monthStatRef);
+
+		transaction.update(achievementRef, {
 			count: (achievementDatum.get('count') || 0) + 1,
 			...(achievementDatum.get('first') === undefined ? {first: user} : {}),
 		});
 
+		transaction.update(categoryStatRef, {count: (categoryStatDatum.get('count') || 0) + 1});
+		transaction.update(difficultyStatRef, {count: (difficultyStatDatum.get('count') || 0) + 1});
+		transaction.update(monthStatRef, {count: (monthStatDatum.get('count') || 0) + 1});
+
 		const oldCounts = userDatum.get('counts') || {};
-		await transaction.update(userRef, {
+		transaction.update(userRef, {
 			counts: {
 				...oldCounts,
 				[category]: (oldCounts[category] || 0) + 1,
