e807a34efdbb1741f548f165e6b5f99304a9962a
Koki Takahashi <hakatasiloving@gmail.com>
2024-04-23 08:03:44 +0900

    Fix build by using webpack v4 as primary builder

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
 	loading: {color: '#fff'},
 
 	css: [
-		'bulma/css/bulma.css',
+		'bulma',
 		'bulma-o-steps/bulma-steps.css',
 		'bulma-timeline/dist/css/bulma-timeline.min.css',
 		'remixicon/fonts/remixicon.css',
@@ -35,10 +35,8 @@ export default {
 
 	build: {
 		postcss: {
-			preset: {
-				features: {
-					customProperties: false,
-				},
+			features: {
+				'custom-properties': false,
 			},
 		},
 	},
